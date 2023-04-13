b86a2d0ea0456a91c68a03948cc516c805d225b1
Douglas Mendizábal <dmendiza@redhat.com>
2023-04-13 10:35:24 -0400

    Initial BarbicanAPI spec definition

--- api/v1beta1/barbicanapi_types.go
+++ api/v1beta1/barbicanapi_types.go
@@ -25,11 +25,76 @@ import (
 
 // BarbicanAPISpec defines the desired state of BarbicanAPI
 type BarbicanAPISpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of BarbicanAPI. Edit barbicanapi_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default=barbican
+	// ServiceUser - optional username used for this service to register in keystone
+	ServiceUser string `json:"serviceUser"`
+
+	// +kubebuilder:validation:Required
+	// BarbicanAPI Container Image URL
+	ContainerImage string `json:"containerImage"`
+
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:validation:Enum=internal;external
+	// +kubebuilder:default=external
+	APIType string `json:"apiType"`
+
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default=1
+	// +kubebuilder:validation:Maximum=32
+	// +kubebuilder:validation:Minimum=0
+	// Replicas of Barbican API to run
+	Replicas int32 `json:"replicas"`
+
+	// +kubebuilder:validation:Optional
+	// DatabaseHostname - Barbican Database Hostname
+	DatabaseHostname string `json:"databaseHostname"`
+
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default=barbican
+	// DatabaseUser - optional username used for barbican DB, defaults to barbican
+	DatabaseUser string `json:"databaseUser"`
+
+	// +kubebuilder:validation:Optional
+	// Secret containing all passwords / keys needed
+	Secret string `json:"secret"`
+
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default={database: BarbicanDatabasePassword,
+	//                       service: BarbicanServiceUserPassword}
+	// TODO(dmendiza): Maybe we'll add SimpleCrypto key here?
+	// PasswordSelectors - Selectors to identify the DB and ServiceUser password from the Secret
+	PasswordSelectors PasswordSelector `json:"passwordSelectors"`
+
+	// +kubebuilder:validation:Optional
+	// NodeSelector to target subset of worker nodes running this service
+	NodeSelector map[string]string `json:"nodeSelector,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	// Debug - enable debug for different deploy stages. If an init container is used, it runs and the
+	// actual action pod gets started with sleep infinity
+	Debug BarbicanAPIDebug `json:"debug,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	// CustomServiceConfig - customize the service config using this parameter to change service defaults,
+	// or overwrite rendered information using raw OpenStack config format. The content gets added to
+	// to /etc/<service>/<service>.conf.d directory as custom.conf file.
+	CustomServiceConfig string `json:"customServiceConfig,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	// ConfigOverwrite - interface to overwrite default config files like e.g. logging.conf or policy.json.
+	// But can also be used to add additional files. Those get added to the service config dir in /etc/<service> .
+	// TODO(dmendiza): -> implement
+	DefaultConfigOverwrite map[string]string `json:"defaultConfigOverwrite,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	// NetworkAttachments is a list of NetworkAttachment resource names to expose the services to the given network
+	NetworkAttachments []string `json:"networkAttachments,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	// ExternalEndpoints, expose a VIP via MetalLB on the pre-created address pool
+	ExternalEndpoints []MetalLBConfig `json:"externalEndpoints,omitempty"`
 }
 
 // BarbicanAPIStatus defines the observed state of BarbicanAPI
