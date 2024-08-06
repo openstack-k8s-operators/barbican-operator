8521f7b4d6cf98e779513256ce611985e8f6a825
Martin Schuppert <mschuppert@redhat.com>
2024-08-06 17:14:43 +0200

    Return nil error in case NotFound and to use RequeueAfter
    Currently the reconciler returned both a non-zero result and a
    non-nil error.
    The result will always be ignored if the error is non-nil and the
    non-nil error causes reqeueuing with exponential backoff.
    
    In case of NotFound return nil that the ReqeueAfter is used.
    
    For more details, see: https://pkg.go.dev/sigs.k8s.io/controller-runtime/pkg/reconcile#Reconciler
    
    Signed-off-by: Martin Schuppert <mschuppert@redhat.com>

--- controllers/barbicanworker_controller.go
+++ controllers/barbicanworker_controller.go
@@ -172,12 +172,13 @@ func (r *BarbicanWorkerReconciler) getSecret(
 	secret, hash, err := secret.GetSecret(ctx, h, secretName, instance.Namespace)
 	if err != nil {
 		if k8s_errors.IsNotFound(err) {
+			h.GetLogger().Info(fmt.Sprintf("secret %s not found", secretName))
 			instance.Status.Conditions.Set(condition.FalseCondition(
 				condition.InputReadyCondition,
 				condition.RequestedReason,
 				condition.SeverityInfo,
 				condition.InputReadyWaitingMessage))
-			return ctrl.Result{RequeueAfter: time.Duration(10) * time.Second}, fmt.Errorf("secret %s not found", secretName)
+			return ctrl.Result{RequeueAfter: time.Duration(10) * time.Second}, nil
 		}
 		instance.Status.Conditions.Set(condition.FalseCondition(
 			condition.InputReadyCondition,
@@ -436,13 +437,14 @@ func (r *BarbicanWorkerReconciler) reconcileNormal(ctx context.Context, instance
 		_, err := nad.GetNADWithName(ctx, helper, netAtt, instance.Namespace)
 		if err != nil {
 			if k8s_errors.IsNotFound(err) {
+				Log.Info(fmt.Sprintf("network-attachment-definition %s not found", netAtt))
 				instance.Status.Conditions.Set(condition.FalseCondition(
 					condition.NetworkAttachmentsReadyCondition,
 					condition.RequestedReason,
 					condition.SeverityInfo,
 					condition.NetworkAttachmentsReadyWaitingMessage,
 					netAtt))
-				return ctrl.Result{RequeueAfter: time.Second * 10}, fmt.Errorf("network-attachment-definition %s not found", netAtt)
+				return ctrl.Result{RequeueAfter: time.Second * 10}, nil
 			}
 			instance.Status.Conditions.Set(condition.FalseCondition(
 				condition.NetworkAttachmentsReadyCondition,
