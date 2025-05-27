3d68ff1e1256d9b7e5821c21468b9034e3801898
Rajesh Tailor <ratailor@redhat.com>
2025-05-27 15:35:49 +0530

    Do not update status when reconciler exits due to panic
    Currently we use deferred call to always update the status
    field when Reconcile call exits, which seems correct for
    any other error except when Reconciler exits due to panic.
    
    This change adds a call to recover function and try to
    handle panic and log error.
    
    Closes: OSPRH-16971

--- controllers/barbicanworker_controller.go
+++ controllers/barbicanworker_controller.go
@@ -116,6 +116,11 @@ func (r *BarbicanWorkerReconciler) Reconcile(ctx context.Context, req ctrl.Reque
 	// Always patch the instance status when exiting this function so we can
 	// persist any changes.
 	defer func() {
+		// Don't update the status, if reconciler Panics
+		if r := recover(); r != nil {
+			Log.Info(fmt.Sprintf("panic during reconcile %v\n", r))
+			panic(r)
+		}
 		condition.RestoreLastTransitionTimes(
 			&instance.Status.Conditions, savedConditions)
 		if instance.Status.Conditions.IsUnknown(condition.ReadyCondition) {
